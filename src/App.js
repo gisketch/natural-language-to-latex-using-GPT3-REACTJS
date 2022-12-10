f9b4a2c494e25cb943800ed382cd3eb536af0fbe
gisketch <agjmnz@gmail.com>
2022-12-10 23:23:43 +0800

    started working on the skeleton front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import LatexRenderer from './components/LatexRenderer';
+import Button from './components/Button';
+import { useState, useEffect, React } from 'react';
+
 
 function App() {
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='Container'>
+        <div  className='Video'>
+        </div>
+        <div className='Panel'>
+          <div className='Info'>
+            <div className='Title'>
+              <p>Input:</p>
+            </div>
+            <div className='Answer'>
+              <p>Here's your equation!</p>
+              <LatexRenderer latex="$\left [ \frac{a}{b} \right ]$" />
+            </div>
+            <div className='Inputs'>
+              <input type="text" name="problem"></input>
+              <Button label="Generate" />
+            </div>
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/LatexRenderer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'katex/dist/katex.min.css'
+import Latex from 'react-latex-next';
+
+function LatexRenderer(props) {
+  return (
+    <h3>
+      <Latex>
+      {props.latex}
+      </Latex>
+    </h3>
+  );
+}
+
+export default LatexRenderer;
